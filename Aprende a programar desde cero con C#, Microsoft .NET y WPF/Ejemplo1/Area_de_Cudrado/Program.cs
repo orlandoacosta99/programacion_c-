89da4cd2dd5b2412049eb2461593b7366bcdb542
Orlando Acosta <acostavargasorlando99@gmail.com>
2023-10-27 08:44:17 -0500

    Se añadieron ficheros

---
+++ Aprende a programar desde cero con C#, Microsoft .NET y WPF/Ejemplo1/Area_de_Cudrado/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Area_de_Cudrado
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string userData;
+			double side;
+			double squareArea;
+			Console.WriteLine("Hola, te proporcionaré el área de un cuadrado.");
+			Console.WriteLine(" ______");
+			Console.WriteLine("|      |");
+			Console.WriteLine("|      |");
+			Console.WriteLine("|______|");
+			Console.WriteLine("¿Cuánto mide un lado del cuadrado?");
+			userData = Console.ReadLine();
+
+			if (double.TryParse(userData, out side))
+			{
+				squareArea = side * side;
+				Console.WriteLine($"El área de tú cuadrado es: {squareArea}");
+			}
+			else
+			{
+				Console.WriteLine("Los datos proporcionados son incorrectos.");
+			}
+			Console.ReadKey();
+		}
+	}
+}
---
+++ Aprende a programar desde cero con C#, Microsoft .NET y WPF/Ejemplo1/Area_de_Triangulo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Drawing;
+
+namespace Area_de_Triangulo
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string userBase;
+			string userAltura;
+			double sideBase;
+			double sideAltura;
+			double squareArea;
+			Console.WriteLine("Hola, encontraremos el area de un triangulo");
+			Console.WriteLine("Ingresa la base del cuadrado: ");			
+			userBase = Console.ReadLine();
+			Console.WriteLine("Ingresa la altura del cuadrado: ");
+			userAltura = Console.ReadLine();
+
+
+			if (double.TryParse(userBase, out sideBase) && double.TryParse(userAltura, out sideAltura))
+			{
+				squareArea = sideBase * sideAltura/2;
+
+				Console.WriteLine($"El área del triangulo es: {squareArea}");
+			}
+			else
+			{
+				Console.WriteLine("Los datos proporcionados son incorrectos.");
+			}
+			Console.ReadKey();
+
+		}
+	}
+}
